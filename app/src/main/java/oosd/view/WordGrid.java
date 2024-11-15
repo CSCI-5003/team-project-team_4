f267a762fcd3720d7071f87c6e3d3f55cde6784c
William Phoenix <will@Williams-MacBook-Pro.local>
2024-11-14 18:02:53 -0600

    added alreadyGuessed datamember to WordGrid class & added set & get methods

--- app/src/main/java/oosd/view/WordGrid.java
+++ app/src/main/java/oosd/view/WordGrid.java
@@ -1,16 +1,32 @@
 package oosd.view;
 import javax.swing.JPanel;
 
+import java.util.ArrayList;
+
 import oosd.model.WordGroup;
 
 public class WordGrid extends JPanel {
     
     private Word[] buttons = new Word[16];
     private WordGroup[] wordGroups;
+    private ArrayList<WordGroup> alreadyGuessed;
     
     public WordGrid(WordGroup[] wordGroups) {
 
         this.wordGroups = wordGroups;
+        alreadyGuessed = new ArrayList<WordGroup>();
 
+    }
+
+    public WordGroup[] getWordGroups() {
+        return this.wordGroups;
+    }
+
+    public ArrayList<WordGroup> getAlreadyGuessed() {
+        return this.alreadyGuessed;
+    }
+
+    public void addGuess(WordGroup guess) {
+        alreadyGuessed.add(guess);
     }
 }
